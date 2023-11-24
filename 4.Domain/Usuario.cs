2d8e986cc6ba538536554c8c1bedbdcc6dafff34
Alexis Dorado Muñoz <alexis.dorado.munoz@gmail.com>
2023-11-24 16:39:13 -0500

    Proyectos con sus referencias iniciales

---
+++ 4.Domain/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Domain
+{
+    public class Usuario : IdentityUser
+    {
+        [Key]
+        public Guid Identificador { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+    }
+}
